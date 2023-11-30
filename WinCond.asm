2a47b7039f26bc60ec0b2ebb1e276d8f3db3bdaa
Ahsanawan123 <98630461+Ahsanawan123@users.noreply.github.com>
2023-11-29 18:13:51 -0600

    Update WinCond.asm
    - Fixed the Win and draw functions, should work when called (see added main2 file in test-files branch to test this and understand)
    -continues process if valid move is found
    - changes made on 11/29/23, uploaded at 6:13PM

--- WinCond.asm
+++ WinCond.asm
@@ -3,6 +3,7 @@ PlayerWinMssg: .asciiz "Player Wins!\n"
 CompWinMssg: .asciiz "Computer Wins!\n"
 DrawMssg: .asciiz "It's a draw!\n"
 
+
 .text
 
 # Part a
@@ -20,22 +21,25 @@ checkHorizontalWin:
     # $t7 - constant value 4 for comparison
 # Function: Check for a horizontal win
 
+    # Load the turn into $t5 to be used in win-checking functions
+    lw $t5, turn
+    
     la $t0, status    # Load base address of the status array
     li $t1, 0              # Row counter
     li $t7, 4              # Count for a win condition
 
 horizontal_row_loop:
-    ble $t1, 5, end_horizontal_check  # Check all rows
+    bge $t1, 6, end_horizontal_check  # Check all rows
     li $t2, 0              # Column counter
 
 horizontal_column_loop:
-    ble $t2, 2, next_row_horizontal   # Check up to the 3rd column for four in a row
+    bgt $t2, 3, next_row_horizontal   # Check up to the 3rd column for four in a row
+    
     li $t4, 0              # Reset consecutive count
-
-    # Loop to check four consecutive elements in a row
     li $t3, 0              # Inner loop counter for checking four in a row
     check_consecutive_horizontal:
-        bge $t3, 4, continue_horizontal  # Check four positions
+        bgt $t3, 3, continue_horizontal  # Check four positions
+        #j next_column_horizontal  # If four have been checked and no win, go to the next column
 
         # Calculate index in status
         mul $t6, $t1, 6     # Row width
@@ -45,54 +49,60 @@ horizontal_column_loop:
         add $t6, $t0, $t6   # Address in the status
 
         lw $t8, 0($t6)      # Load the element
-        lw $t9,turn           # Assume we are checking for player's turn (1). Change to 2 for computer.
-        bne $t8, $t9, reset_consecutive_horizontal # Reset if not matching player's marker
+        bne $t8, $t5, reset_consecutive_horizontal # Reset if not matching player's marker
+         
         addi $t4, $t4, 1    # Increment consecutive count
-
         addi $t3, $t3, 1    # Move to the next element
         j check_consecutive_horizontal
 
     continue_horizontal:
-        beq $t4, $t7, win_horizontal # Check if four in a row is reached
-        addi $t2, $t2, 1    # Move to the next column
-        j horizontal_column_loop
+        bne $t4, $t7, next_column_horizontal # Move to next column if four in a row not reached
+        j win_horizontal    # Jump to win condition if four in a row
 
     reset_consecutive_horizontal:
-        li $t4, 0           # Reset consecutive count
+        li $t4, 0         # Reset consecutive count
         j continue_horizontal
-
+        
+next_column_horizontal:
+        addi $t2, $t2, 1  # Move to the next column
+        j horizontal_column_loop
+        
 next_row_horizontal:
-    addi $t1, $t1, 1      # Move to the next row
+     addi $t1, $t1, 1    # Move to the next row
     j horizontal_row_loop
 
 end_horizontal_check:
-    li $v0, 0             # No win found
-    j checkForDraw
+    # No win found, jump to the next check
+    j checkVerticalWin
 
 win_horizontal:
-    lw $t9, turn               # Win found
-    beq $t9, 1, playerWin
-    beq $t9, 2, compWin
+    # Compare turn and decide the message to print            
+     beq $t5, 1, playerWin        # Win found
+     beq $t5, 2, compWin
+     j exitProgram
+       
        
 # Function: Check for a vertical win
 .globl checkVerticalWin
 checkVerticalWin:
+    lw $t5, turn
+
     la $t0, status   # Load base address of the status array
     li $t1, 0             # Column counter
     li $t7, 4             # Count for a win condition
 
 vertical_column_loop:
-    ble $t1, 5, end_vertical_check   # Check all columns
+    bgt $t1, 6, end_vertical_check   # Check all columns
     li $t2, 0             # Row counter
 
 vertical_row_loop:
-    ble $t2, 2, next_column_vertical # Check up to the 3rd row for four in a row
+    bgt $t2, 3, next_column_vertical # Check up to the 3rd row for four in a row
     li $t4, 0             # Reset consecutive count
 
     # Loop to check four consecutive elements in a column
     li $t3, 0             # Inner loop counter for checking four in a row
     check_consecutive_vertical:
-        bge $t3, 4, continue_vertical # Check four positions
+        bgt $t3, 18, continue_vertical # Check four positions
 
         # Calculate index in status
         mul $t6, $t2, 6    # Row width
@@ -102,8 +112,8 @@ vertical_row_loop:
         add $t6, $t0, $t6  # Address in the status
 
         lw $t8, 0($t6)     # Load the element
-        lw $t9, turn          # Assume we are checking for player's turn (1). Change to 2 for computer.
-        bne $t8, $t9, reset_consecutive_vertical # Reset if not matching player's marker
+        #lw $t9, turn          # Assume we are checking for player's turn (1). Change to 2 for computer.
+        bne $t8, $t5, reset_consecutive_vertical # Reset if not matching player's marker
         addi $t4, $t4, 1   # Increment consecutive count
 
         addi $t3, $t3, 6   # Move to the next element in the column
@@ -111,12 +121,16 @@ vertical_row_loop:
 
     continue_vertical:
         beq $t4, $t7, win_vertical # Check if four in a row is reached
-        addi $t2, $t2, 1   # Move to the next row
-        j vertical_row_loop
+        # addi $t2, $t2, 1   # Move to the next row
+        j next_row_vertical
 
     reset_consecutive_vertical:
         li $t4, 0          # Reset consecutive count
         j continue_vertical
+
+next_row_vertical:
+	addi $t2, $t2, 1  # Move to the next row
+        j vertical_row_loop
 
 next_column_vertical:
     addi $t1, $t1, 1     # Move to the next column
@@ -124,24 +138,23 @@ next_column_vertical:
 
 end_vertical_check:
     li $v0, 0            # No win found
-    j checkForDraw
+    j checkDiagonalWinAscending       # might have to jump to next function if no win is found
 
 win_vertical:
     lw $t9, turn
     beq $t9, 1, playerWin
     beq $t9, 2, compWin
+    j exitProgram
     
     
 # Function: Check for an ascending diagonal win
 .globl checkDiagonalWinAscending
 checkDiagonalWinAscending:
     la $t0, status   # Load base address of the status array
-    lw $t5, turn             # Assuming it's the player's turn (use 2 for the computer)
-    li $t7, 4             # Count for a win condition
+    lw $t5, turn     # Load the current player's number
 
-    # Start from the first possible index of the lowest ascending diagonal
-    # that can have at least four cells (adjust the index based on your mapping)
-    li $t1, 15            # Example start index for ascending diagonals
+    li $t1, 30       # Starting index for the first diagonal check from bottom left
+    li $t7, 4        # Count for a win condition
 
     ascending_diagonal_loop:
         li $t4, 0         # Reset consecutive count
@@ -150,14 +163,13 @@ checkDiagonalWinAscending:
         check_consecutive_ascending:
             bge $t3, $t7, next_diagonal_ascending # Check four positions
 
-            # Calculate index in status for ascending diagonal
             mul $t6, $t3, 5  # Index increases by 5 for each step in ascending diagonal
-            add $t6, $t6, $t1
+    	    sub $t6, $t1, $t6 # Subtract since we're moving up and to the right
             sll $t6, $t6, 2  # Convert to word offset
             add $t6, $t0, $t6 # Address in the status
 
             lw $t8, 0($t6)   # Load the element
-            bne $t8, $t5, ascending_check_next # If not matching, check next
+            bne $t8, $t5, reset_consecutive_descending # If not matching, check next
             addi $t4, $t4, 1 # Increment consecutive count
 
             addi $t3, $t3, 1 # Move to the next element in the diagonal
@@ -165,27 +177,26 @@ checkDiagonalWinAscending:
 
         next_diagonal_ascending:
             beq $t4, $t7, win_diagonal_ascending # Check if four in a row is reached
-            sub $t1, $t1, 6    # Move to the next diagonal start index
-            bgez $t1, ascending_diagonal_loop # If index is valid, check next diagonal
+            addi $t1, $t1, 1   # Move to the next starting index to the right
+    	    blt $t1, 33, ascending_diagonal_loop # If index less than 33, continue checking
 
-         ascending_check_next:
-            addi $t1, $t1, 1   # Move to the next column to start checking next diagonal
-            bltz $t1, end_ascending_check # If we've checked all columns, we're done
-            j ascending_diagonal_loop # Otherwise, continue checking next diagonal
+        check_next:
+    	addi $t1, $t1, 6   # Move up to start checking next diagonal
+    	blt $t1, 36, ascending_diagonal_loop # If index less than 36, continue checking
 
-    end_ascending_check:
-        li $v0, 0          # No win found
-        j checkForDraw
+    	li $v0, 0          # No win found
+    	j checkDiagonalWinDescending    # Proceed to check for a draw
 
 win_diagonal_ascending:
      lw $t9, turn             # Win found
      beq $t9, 1, playerWin
      beq $t9, 2, compWin
-     
+     j exitProgram
 
 # Function to check for a descending diagonal win
 .globl checkDiagonalWinDescending
 checkDiagonalWinDescending:
+    
     # Function: Check for a descending diagonal win
     la $t0, status   # Load base address of the status array
     lw $t5, turn        # Assuming it's the player's turn (use 2 for the computer)
@@ -200,41 +211,40 @@ checkDiagonalWinDescending:
     	li $t3, 0         # Inner loop counter for checking four in a diagonal
     
     check_consecutive_descending:
-        bge $t3, $t7, end_check_descending  # Check four positions
+        bge $t3, $t7, next_diagonal_descending  # Check four positions
         
         # Calculate index in status for descending diagonal
-        mul $t8, $t3, 7  # Index increases by 7 for each step in descending diagonal
-        add $t8, $t8, $t1
-        sll $t8, $t8, 2  # Convert to word offset
-        add $t8, $t0, $t8 # Address in the status
+        mul $t6, $t3, 7  # Index increases by 7 for each step in descending diagonal
+        add $t6, $t6, $t1
+        sll $t6, $t6, 2  # Convert to word offset
+        add $t6, $t0, $t6 # Address in the status
         
-        lw $t9, 0($t8)   # Load the element
-        bne $t9, $t5, descending_check_next # If not matching, check next
+        lw $t8, 0($t6)   # Load the element
+        bne $t8, $t5, reset_consecutive_descending # If not matching, check next
         addi $t4, $t4, 1 # Increment consecutive count
         
         addi $t3, $t3, 1 # Move to the next element in the diagonal
         j check_consecutive_descending
         
-    end_check_descending:
-        beq $t4, $t7, win_diagonal_descending # Check if four in a row is reached
-        addi $t1, $t1, 1    # Move to the next row to start checking next diagonal
-        blt $t1, 6, descending_diagonal_loop # If index is valid, check next diagonal
-        
-    descending_check_next:
-        # Once we've checked the first column, move to the next potential start index
-        addi $t1, $t1, 6   # Move to the next row to start checking next diagonal
-        blt $t1, 18, descending_diagonal_loop # If index is valid, check next diagonal
-        j end_descending_check # Otherwise, we've checked all possible diagonals
+    next_diagonal_descending:
+	beq $t4, $t7, win_diagonal_descending # Check if four in a row is reached
 
-    end_descending_check:
+	# Decide whether to move right or left for the next check
+    	addi $t1, $t1, 1
+    	blt $t1, 18, descending_diagonal_loop # If index less than 18, check the next diagonal
+    
         li $v0, 0          # No win found
         j checkForDraw     # Jump to check for draw before returning
-        
+
+reset_consecutive_descending:
+    li $t4, 0          # Reset consecutive count
+    j next_diagonal_descending
+
 win_diagonal_descending:
-     lw $t9, turn             # Win found
-     beq $t9, 1, playerWin
-     beq $t9, 2, compWin
-     
+    lw $t9, turn       # Win found
+    beq $t9, 1, playerWin
+    beq $t9, 2, compWin
+    j exitProgram      # Exit the program after a win
 playerWin:
     li $v0, 4                  # syscall for print string
     la $a0, PlayerWinMssg
@@ -250,6 +260,7 @@ compWin:
 exitProgram:
     li $v0, 10                 # syscall for exit
     syscall
+  
     
 # Part b
 # Validates move and checks for draw
@@ -312,12 +323,13 @@ check_draw:
 
 valid_move_found:
     # If a valid move is found, simply return
-    j jumpToMain
+    j jumpToMain #jr ra
     
 # Part c
 .globl jumpToMain
 jumpToMain:
-    j main         # Jump to the main label
+    #j main         # Jump to the main label
+    jr $ra
     nop            # Following the jump with a nop is a good practice to handle the delay slot
 
 
